dae54d01762ce56044e0649b05ed66ca0e2fe1a1
Gerald Thibault <dieselmachine@gmail.com>
2013-06-29 10:13:16 +0000

    removed debug printing

--- baph/auth/mixins.py
+++ baph/auth/mixins.py
@@ -107,7 +107,6 @@ class UserPermissionMixin(object):
         return perms
 
     def get_resource_permissions(self, resource, action=None):
-        print 'get_res_perms:', resource, action
         if resource and not action:
             assert False
         if not resource:
@@ -116,10 +115,8 @@ class UserPermissionMixin(object):
         if resource not in perms:
             return set()
         perms = perms.get(resource, {})
-        print perms
         if action:
             perms = perms.get(action, {})
-        print 'permsL', perms, action
         return perms
     
     def has_resource_perm(self, resource):
@@ -137,11 +134,6 @@ class UserPermissionMixin(object):
         cls_name = tuple(perms)[0].base_class
         cls = Base._decl_class_registry[cls_name]
         obj = cls(**filters)
-        #idkey = identity_key(instance=obj)
-        #session = orm.sessionmaker()
-        #if not idkey in session.identity_map:
-        #    print 'has_perm: obj not in session, querying db'
-        #print resource, action, idkey
         return self.has_obj_perm(resource, action, obj)
 
 
@@ -152,8 +144,6 @@ class UserPermissionMixin(object):
         ctx = self.get_context()
         perms = self.get_resource_permissions(resource, action)
         if not perms:
-            #assert False
-            print 'no perms', resource, action, obj
             return False
 
         perm_map = {}
@@ -169,17 +159,13 @@ class UserPermissionMixin(object):
                 col = prop.local_remote_pairs[0][0]
                 col_attr = column_to_attr(type(obj), col)
                 if not col_attr.key in perm_map:
-                    print 'attr.key:', col_attr.key
                     perm_map[col_attr.key] = set([None])
 
-        print 'perm map:', perm_map, action
         for k,v in perm_map.items():
-            print 'k,v:', k, v
             frags = k.split('.')
             col_name = frags.pop()
             current_obj = obj
             while frags:
-                print '\tcurrent_obj', current_obj
                 attr_name = frags.pop(0)
                 previous_obj = current_obj
                 if not previous_obj:
@@ -188,14 +174,12 @@ class UserPermissionMixin(object):
                 if current_obj is None:
                     # relation was empty, let's try manually grabbing via pk
                     attr = getattr(type(previous_obj), attr_name)
-                    print '\tattr:', attr
                     prop = attr.property
                     rel_cls = prop.argument
                     if isinstance(rel_cls, type(lambda x: x)):
                         rel_cls = rel_cls()
                     rel_col = prop.local_remote_pairs[0][0]
                     rel_key = rel_col.key
-                    print '\trel_cls/col/key:', rel_cls, rel_col, rel_key
                     # we need to find the attr which refers to this column
                     # TODO: is there a better way?
                     for attr_ in inspect(type(previous_obj)).all_orm_descriptors:
@@ -206,27 +190,21 @@ class UserPermissionMixin(object):
                         except:
                             pass
                     rel_val = getattr(previous_obj, rel_key, None)
-                    print '\trel_val:', rel_val
                     if rel_val is None:
                         # relation and key are both empty: no parent found
-                        print 'wqut'
                         continue
-                        return True
                     else:
                         session = orm.sessionmaker()
                         current_obj = session.query(rel_cls).get(rel_val)
-                        print '\tnew obj:', current_obj, rel_cls, rel_val
 
             if not current_obj:
                 continue
 
             # now grab final value
             if not hasattr(current_obj, col_name):
-                print 'skipping', current_obj, col_name
                 continue
 
             value = getattr(current_obj, col_name)
-            print 'value:', current_obj, col_name, value, v
             if action == 'add':
                 # for adding items, all filters must match
                 if value and str(value) not in v:
@@ -239,80 +217,3 @@ class UserPermissionMixin(object):
                     return True
             
         return action == 'add'
-'''
-class ModelPermissionMixin(object):
-
-    def get_context(self):
-        ctx = {}
-        for key,attr in inspect(self.__class__).all_orm_descriptors.items():
-            if not attr.is_attribute:
-                continue
-            if type(attr.property) == ColumnProperty:
-                cls_name = self.__class__.__name__.lower()
-                ctx_key = '%s.%s' % (cls_name, key)
-                ctx[ctx_key] = getattr(self, key)
-                continue
-            if type(attr.property) != RelationshipProperty:
-                continue
-            if attr.property.direction.name != 'MANYTOONE':
-                continue
-            if len(attr.property.local_remote_pairs) != 1:
-                continue            
-                
-            parent = getattr(obj, key)
-            if parent:
-                ctx.update(parent.get_context())
-        return ctx
-
-    @classmethod
-    def get_fks(cls, include_parents=True):
-        keys = []
-        cls_name = cls.__name__
-
-        # add permission for unrestricted access
-        keys.append( ('any', None, None, None, cls_name) )
-
-        # add permission for single instance access
-        if len(cls.__mapper__.primary_key) == 1:
-            col_key = cls.__name__.lower() + '_id'
-            key = cls.__mapper__.primary_key[0].key
-            value = '%%(%s)s' % key
-            keys.append( ('single', key, value, col_key, cls_name) )
-
-        for k,v in cls._meta.permission_values.items():
-            key = cls.__mapper__.primary_key[0].key
-            keys.append( (k, key, v, None, cls_name) )
-
-        if not include_parents:
-            return keys
-
-        fks = []
-        for key in getattr(cls._meta.permission_parents):
-            attr = getattr(cls, key)
-            if not attr.is_attribute:
-                continue
-            prop = attr.property
-            if type(prop) != RelationshipProperty:
-                continue
-            if prop.direction.name != 'MANYTOONE':
-                continue
-            if len(prop.local_remote_pairs) != 1:
-                continue
-            
-            col = prop.local_remote_pairs[0][0]
-            sub_cls = prop.argument
-            inc_par = sub_cls != User
-            sub_fks = sub_cls.get_fks(include_parents=inc_par)
-
-            for limiter, key_, value, col_key, base_cls in sub_fks:
-                if not key_:
-                    continue
-                key_ = '%s.%s' % (key, key_)
-                if not col_key:
-                    col_key = col.key
-                if limiter == 'single':
-                    limiter = key_.split('.')[-2]
-                    value = '%%(%s)s' % col_key
-                keys.append( (limiter, key_, value, col_key, cls_name) )
-        return keys
-'''
