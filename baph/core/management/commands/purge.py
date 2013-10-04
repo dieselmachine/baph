220344f2cb36572975b95545bb941743c85ff598
Gerald Thibault <dieselmachine@gmail.com>
2013-10-04 20:17:45 +0000

    .

--- baph/core/management/commands/purge.py
+++ baph/core/management/commands/purge.py
@@ -136,8 +136,8 @@ and DELETE ALL TABLES AND SCHEMAS. Are you sure you want to do this?
             for fkc in all_fks:
                 if verbosity >= 1:
                     self.stdout.write("\t%s\n" % fkc)
-                print conn.execute('show open tables').all()
-                print conn.execute('show engine innodb status').all()
+                print conn.execute('show open tables').fetchall()
+                print conn.execute('show engine innodb status').fetchall()
                 conn.execute(DropConstraint(fkc))
 
             if verbosity >= 1:
